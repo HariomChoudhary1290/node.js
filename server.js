26cc3ff199109fdab4ba78c8cf7ad31db41bd450
Hariom Choudhary1290 <hariom.choudhary015@gmail.com>
2026-06-08 11:06:09 +0530

    Testing Semgrep Tool

--- server.js
+++ server.js
@@ -5,16 +5,23 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Application Running");
+res.send("Application Running");
 });
 
 app.get("/health", (req, res) => {
-    res.status(200).json({
-        status: "UP",
-        service: "secure-nodejs-app"
-    });
+res.status(200).json({
+status: "UP",
+service: "secure-nodejs-app"
+});
+});
+
+// Semgrep Test Route (Intentionally Insecure)
+app.get("/eval", (req, res) => {
+const userInput = req.query.input;
+eval(userInput);
+res.send("Executed");
 });
 
 app.listen(3000, () => {
-    console.log("Server Started");
+console.log("Server Started");
 });
