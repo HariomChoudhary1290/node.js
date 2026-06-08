016e1bd24e92efa93736e49e5c3f8c0248699da7
Hariom Choudhary1290 <hariom.choudhary015@gmail.com>
2026-06-08 11:14:21 +0530

    Re Run Pipeline

--- server.js
+++ server.js
@@ -5,23 +5,16 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-res.send("Application Running");
+    res.send("Application Running");
 });
 
 app.get("/health", (req, res) => {
-res.status(200).json({
-status: "UP",
-service: "secure-nodejs-app"
-});
-});
-
-// Semgrep Test Route (Intentionally Insecure)
-app.get("/eval", (req, res) => {
-const userInput = req.query.input;
-eval(userInput);
-res.send("Executed");
+    res.status(200).json({
+        status: "UP",
+        service: "secure-nodejs-app"
+    });
 });
 
 app.listen(3000, () => {
-console.log("Server Started");
+    console.log("Server Started");
 });
