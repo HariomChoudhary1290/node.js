8cfcc92e4de60e5e2b4cb642565aacc8b1f59be6
Hariom Choudhary1290 <hariom.choudhary015@gmail.com>
2026-06-15 18:56:22 +0530

    Added Cosmos DB integration

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const container = require("./db");
 
 const app = express();
 
@@ -13,6 +14,48 @@ app.get("/health", (req, res) => {
         status: "UP",
         service: "secure-nodejs-app"
     });
+});
+
+// Create User
+app.post("/users", async (req, res) => {
+    try {
+        const { name, role } = req.body;
+
+        const newUser = {
+            id: Date.now().toString(),
+            name,
+            role
+        };
+
+        const { resource } = await container.items.create(newUser);
+
+        res.status(201).json(resource);
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+});
+
+// Get All Users
+app.get("/users", async (req, res) => {
+    try {
+        const querySpec = {
+            query: "SELECT * FROM c"
+        };
+
+        const { resources } = await container.items
+            .query(querySpec)
+            .fetchAll();
+
+        res.status(200).json(resources);
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
 });
 
 app.listen(3000, () => {
